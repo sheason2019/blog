f1bfb30c4fc0475e21d00a5e3b378359744d3bd5
sheason2019 <811627877@qq.com>
2022-11-10 18:54:26 +0800

    hotfix: AlbumController

--- web/src/pages/post/components/album-controller/item.tsx
+++ web/src/pages/post/components/album-controller/item.tsx
@@ -8,7 +8,7 @@ const Item: Component<Props> = (props) => {
   return (
     <div
       onClick={props.onClick}
-      class="flex flex-col w-full px-2 overflow-hidden justify-center items-center border border-blue-500 py-2 md:py-4 rounded-md font-bold cursor-pointer hover:bg-blue-50"
+      class="flex flex-col w-full overflow-hidden justify-center items-center border border-blue-500 py-2 md:py-4 rounded-md font-bold cursor-pointer hover:bg-blue-50"
     >
       {props.children}
     </div>
